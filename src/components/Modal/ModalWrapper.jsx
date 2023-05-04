509fbf10986d99775b647cfaf33272e2e3eed5ae
Pikadorius <karpukea@yandex.by>
2023-05-04 21:35:09 +0300

    added fields to modal

--- src/components/Modal/ModalWrapper.jsx
+++ src/components/Modal/ModalWrapper.jsx
@@ -5,8 +5,11 @@ import Image from 'next/image';
 import close from '../../assets/close.svg'
 import {useDispatch} from 'react-redux';
 import {setModal} from '../../store/modalSlice';
+import {Input} from 'antd';
+import Button from '../Button/Button';
 
 const ModalWrapper = ({title}) => {
+    const {TextArea}=Input;
 
     const dispatch = useDispatch()
     const closeModal = () => dispatch(setModal(false))
@@ -16,11 +19,24 @@ const ModalWrapper = ({title}) => {
             <div className={s.container} onClick={closeModal}>
                 <div className={s.wrapper} onClick={(e) => e.stopPropagation()}>
                     <h2 className={s.title}>{title}</h2>
-                    <Image src={close} alt={'close'} width={14} height={14} className={s.closeBtn} onClick={closeModal}/>
+                    <Image src={close} alt={'close'} width={14} height={14} className={s.closeBtn}
+                           onClick={closeModal}/>
+                    <form className={s.form}>
+                        <p className={s.fieldName}>Your Name <i className={s.mark}>*</i></p>
+                        <Input rootClassName={'antd-input'} placeholder={'Your Name'}/>
+                        <p className={s.fieldName}>Your Email <i className={s.mark}>*</i></p>
+                        <Input rootClassName={'antd-input'} placeholder={'Your Email'}/>
+                        <p className={s.fieldName}>Phone number</p>
+                        <Input rootClassName={'antd-input'} placeholder={'Your Phone'}/>
+                        <p className={s.fieldName}>Your Message</p>
+                        <TextArea placeholder={'Your Message'} className={s.textarea}/>
+                        <button className={s.btn} type={'submit'}>Submit</button>
+                    </form>
                 </div>
             </div>
         </Portal>
-    );
+    )
+        ;
 };
 
 export default ModalWrapper;
