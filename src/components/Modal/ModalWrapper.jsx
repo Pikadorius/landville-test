22178a0a23270d81fa42f774795f2bc7559fcafe
Pikadorius <karpukea@yandex.by>
2023-05-05 11:10:32 +0300

    last fix (try to make phone input)

--- src/components/Modal/ModalWrapper.jsx
+++ src/components/Modal/ModalWrapper.jsx
@@ -6,6 +6,8 @@ import close from '../../assets/close.svg'
 import {useDispatch} from 'react-redux';
 import {setModal} from '../../store/modalSlice';
 import {Input} from 'antd';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 
 const ModalWrapper = ({title}) => {
     const {TextArea} = Input;
@@ -22,16 +24,28 @@ const ModalWrapper = ({title}) => {
                            onClick={closeModal}/>
                     <form className={s.form}>
                         <label className={s.fieldName}>Your Name <i className={s.mark}>*</i>
-                        <Input rootClassName={'antd-input'} placeholder={'Your Name'}/></label>
+                            <Input rootClassName={'antd-input'} placeholder={'Your Name'}/></label>
 
                         <label className={s.fieldName}>Your Email <i className={s.mark}>*</i>
-                        <Input rootClassName={'antd-input'} placeholder={'Your Email'}/></label>
+                            <Input rootClassName={'antd-input'} placeholder={'Your Email'}/></label>
 
                         <label className={s.fieldName}>Phone number
-                        <Input rootClassName={'antd-input'} placeholder={'Your Phone'}/></label>
+                            {/*<Input rootClassName={'antd-input'} placeholder={'Your Phone'} type={'phone'}/></label>*/}
+                            <PhoneInput
+                                specialLabel={''}
+                                country={'us'}
+                                value={'+1'}
+                                containerStyle={{margin: '16px 0 20px'}}
+                                inputClass={s.phoneInput}
+                                buttonClass={s.dropFlag}
+                                searchClass={s.dropFlag}
+                                dropdownClass={s.dropFlag}
+                            />
+                        </label>
+
 
                         <label className={s.fieldName}>Your Message
-                        <TextArea placeholder={'Your Message'} className={s.textarea}/></label>
+                            <TextArea placeholder={'Your Message'} className={s.textarea}/></label>
 
                         <button className={s.btn} type={'submit'}>Submit</button>
                     </form>
